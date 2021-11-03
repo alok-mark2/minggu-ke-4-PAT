11982f36a33c776c77714b7b42d3dafc1362a16b
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2021-11-03 19:02:41 +0700

    Add files via upload

---
+++ ClientProxy-P4-20190140066/ClientProxy-P4-20190140066/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClientProxy_P4_20190140066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.MatematikaClient obj = new
+            ServiceReference1.MatematikaClient();
+
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+
+            ServiceReference1.Koordinat a = new
+            ServiceReference1.Koordinat();
+            ServiceReference1.Koordinat b = new
+            ServiceReference1.Koordinat();
+            a.X = 7;
+            a.Y = 8;
+
+            b.X = 5;
+            b.Y = 6;
+
+            double selisihX = a.X - b.X;
+            double selisihY = a.Y - b.Y;
+            double jarak = Math.Sqrt(Math.Pow(selisihX, 2) + Math.Pow(selisihY, 2));
+            Console.WriteLine("Hasil Koordinat " + jarak);
+            Console.ReadLine();
+        }
+    }
+}
