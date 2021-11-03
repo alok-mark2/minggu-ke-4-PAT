fa946f9c1ed4ea5f376256a8b8f0b9a3edb2be49
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2021-11-03 19:04:16 +0700

    Update Program.cs

--- ClientCFC-P4-066/ClientCFC-P4-066/Program.cs
+++ ClientCFC-P4-066/ClientCFC-P4-066/Program.cs
@@ -10,21 +10,23 @@ namespace ClientCFC_P4_066
 {
     class Program
     {
-        BasicHttpBinding bind = new BasicHttpBinding();
-        ChannelFactory<IMatematika> objChannel = new
-        ChannelFactory<IMatematika>(bind, "http://localhost:1907");
-        IMatematika obj = objChannel.CreateChannel();
-        double hasilTambah = obj.Tambah(1, 2);
-        Console.WriteLine("1 + 2 = " + hasilTambah);
+       static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
             double hasilKurang = obj.Kurang(3, 2);
-        Console.WriteLine("3 - 2 = " + hasilKurang);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
             double hasilKali = obj.Kali(2, 2);
-        Console.WriteLine("2 x 2 = " + hasilKali);
+            Console.WriteLine("2 x 2 = " + hasilKali);
             double hasilBagi = obj.Bagi(2, 2);
-        Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
             Console.ReadLine();
             Koordinat objk = new Koordinat();
 
+        }
     }
-}
 }
